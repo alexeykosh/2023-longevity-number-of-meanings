eef580f87fbc99a0a36192885bf8097a8fef4c82
alexeykosh <alexeykochevoy@gmail.com>
2024-09-10 12:10:33 +0200

    cleaned up analyses

--- analysis.R
+++ analysis.R
@@ -19,19 +19,6 @@ theme_set(theme_bw())
 
 set.seed(42)
 
-# Function to save models
-run_model <- function(expr, path, reuse = TRUE) {
-  path <- paste0(path, ".Rds")
-  if (reuse) {
-    fit <- suppressWarnings(try(readRDS(path), silent = TRUE))
-  }
-  if (is(fit, "try-error")) {
-    fit <- eval(expr)
-    saveRDS(fit, file = path)
-  }
-  fit
-}
-
 ### Pre-reg plot ### 
 # Getting the data
 age_estimation <- read.csv("data/age_estimations.csv")
